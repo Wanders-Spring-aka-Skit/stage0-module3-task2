dee629d1df1657d52ff736790826702ed73fa29f
Wanders-Spring-aka-Skit <avasovazik643@gmail.com>
2022-08-23 14:40:23 +0500

    Errors 2.0

--- src/main/java/lang/print/gaps/task2/AdvancedNamingConvention.java
+++ src/main/java/lang/print/gaps/task2/AdvancedNamingConvention.java
@@ -1,6 +1,6 @@
 package lang.print.gaps.task2;
 
-public class Advanced_naming_convention {
+public class AdvancedNamingConvention {
     private static final int ADULT_AGE = 18;
 
     private int age;
